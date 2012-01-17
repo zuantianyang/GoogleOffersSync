d4b1df6cc81d0040192dda01e55dd6c43d6cba6f
Hugo Arregui <hugo.arregui@gmail.com>
2012-01-16 23:43:08 -0300

    issue ar-108

--- metadata/schema.sql
+++ metadata/schema.sql
@@ -1,9 +1,16 @@
-
 drop table if exists google_offers CASCADE;
 
 create table google_offers (
   tippr_offer_id varchar(100),
   status varchar(100),
+  last_update date
+);
+
+drop table if exists redemption_codes CASCADE;
+
+create table redemption_codes (
+  size int,
+  status varchar(20),
   last_update date
 );
 
--- sync.py
+++ sync.py
@@ -30,6 +30,13 @@ SECRETS_FILE = 'metadata/client_secrets.json'
 
 logger = logging.getLogger('googleoffers-sync')
 
+from commons.persistence import insert                                                                                         
+from datetime import date
+
+def find_status_size(g_client, pid, status):
+    codes = g_client.GetRedemptionCodesWithStatus(pid, status)
+    return len(codes.get('offer', {}).get('codes', []))
+
 from commons.configuration import open_connection
 conn = open_connection()
 cursor = conn.cursor()
@@ -39,13 +46,18 @@ try:
     g_client = GoogleOffers('8793954', TOKEN_FILE, SECRETS_FILE)
     promotions = tippr_client.find_promotions()
 
+    redemtion_data = dict()
     for i, promotion in enumerate(promotions):
         if promotion['status'] in ['approved', 'active', 'closed']:
             try:
-                status = g_client.GetOfferStatus(promotion['id'])
+                pid = promotion['id']
+                status = g_client.GetOfferStatus(pid)
+            
+                #if status in ['active']: #TODO
+                for status in ['CREATED', 'PURCHASED', 'REDEEMED', 'REFUND_HOLD', 'REFUNDED', 'CANCELLED']:
+                    size = find_status_size(g_client, pid, status)
+                    redemtion_data[status] = redemtion_data.get(status, 0) + size
 
-                from commons.persistence import insert                                                                                         
-                from datetime import date
                 last_update_date = date.today() #TODO
                 data = {
                         'tippr_offer_id': promotion['id'],
@@ -59,6 +71,15 @@ try:
             except GoogleOffersError:
                 pass
 
+    last_update_date = date.today()
+    for status in ['CREATED', 'PURCHASED', 'REDEEMED', 'REFUND_HOLD', 'REFUNDED', 'CANCELLED']:
+        data = {
+                'size'       : redemtion_data[status],
+                'status'     : status,
+                'last_update': last_update_date,
+                }
+        insert(cursor, 'redemption_codes', data.keys(), data)
+        conn.commit()
 except Exception, e:
     logging.exception("Error in google offers sync")
 
