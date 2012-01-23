bfe1639127e3e3c72e0f72ca62eef629d77d1b19
Txema Monzon <txema.monzon@gmail.com>
2012-01-23 14:50:58 -0300

    updated schema.sql

--- metadata/schema.sql
+++ metadata/schema.sql
@@ -1,16 +1,42 @@
-drop table if exists google_offers CASCADE;
+/*
+ Navicat Premium Data Transfer
 
-create table google_offers (
-  tippr_offer_id varchar(100),
-  status varchar(100),
-  last_update date
-);
+ Source Server         : PostgreSQL @ tesla
+ Source Server Type    : PostgreSQL
+ Source Server Version : 90004
+ Source Host           : localhost
+ Source Database       : googleoffers
+ Source Schema         : public
 
-drop table if exists redemption_codes CASCADE;
+ Target Server Type    : PostgreSQL
+ Target Server Version : 90004
+ File Encoding         : utf-8
 
-create table redemption_codes (
-  size int,
-  status varchar(20),
-  last_update date
-);
+ Date: 01/23/2012 14:48:54 PM
+*/
 
+-- ----------------------------
+--  Table structure for "promotions"
+-- ----------------------------
+DROP TABLE IF EXISTS "promotions";
+CREATE TABLE "promotions" (
+	"promotion_id" varchar(100),
+	"status" varchar(100),
+	"last_update" timestamp(6) NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "promotions" OWNER TO "postgres";
+
+
+-- ----------------------------
+--  Table structure for "redemption_codes"
+-- ----------------------------
+DROP TABLE IF EXISTS "redemption_codes";
+CREATE TABLE "redemption_codes" (
+	"size" int4,
+	"status" varchar(20),
+	"last_update" timestamp(6) NULL,
+	"promotion_id" varchar(100)
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "redemption_codes" OWNER TO "postgres";
